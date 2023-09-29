93bb08c90ff381198590b55602ca5451449640d7
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2023-09-29 23:49:35 +0800

    Initial commit- uploading the FINAL project

--- app/yourcart/page.jsx
+++ app/yourcart/page.jsx
@@ -443,7 +443,7 @@ const Cart = () => {
               <div className="cart-list-items">
                 <button
                   type="button"
-                  onClick={() => handleDeleteTemp()}
+                  onClick={() => handleDeleteTemp(item)}
                   className="desktop-btn button-item-cart bg-white rounded-md p-2 inline-flex items-center justify-center text-blue-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
                 >
                   <span className="sr-only">Close menu</span>
