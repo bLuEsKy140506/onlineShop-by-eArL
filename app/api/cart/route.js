b9facf0f6c96d00024b61d69a9e0f0f5772d9a7e
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2023-09-30 13:28:56 +0800

    Initial commit- uploading the FINAL project

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -13,26 +13,26 @@ export const GET = async (request) => {
   }
 };
 
-// export const PATCH = async (request, { params }) => {
-//   const { items } = await request.json();
-//   console.log(params.id, items);
-//   try {
-//     await connectToDB();
-//     console.log(params.id);
-//     // Find the existing prompt by ID
-//     const existingPrompt = await Prompt.findById(params.id);
+export const PATCH = async (request, { params }) => {
+  const { items } = await request.json();
+  console.log(params.id, items);
+  try {
+    await connectToDB();
+    console.log(params.id);
+    // Find the existing prompt by ID
+    const existingPrompt = await Prompt.findById(params.id);
 
-//     if (!existingPrompt) {
-//       return new Response("Prompt not found", { status: 404 });
-//     }
+    if (!existingPrompt) {
+      return new Response("Prompt not found", { status: 404 });
+    }
 
-//     // Update the prompt with new data
-//     existingPrompt.items = items;
+    // Update the prompt with new data
+    existingPrompt.items = items;
 
-//     await existingPrompt.save();
+    await existingPrompt.save();
 
-//     return new Response("Successfully updated the Prompts", { status: 200 });
-//   } catch (error) {
-//     return new Response("Error Updating Prompt", { status: 500 });
-//   }
-// };
+    return new Response("Successfully updated the Prompts", { status: 200 });
+  } catch (error) {
+    return new Response("Error Updating Prompt", { status: 500 });
+  }
+};
